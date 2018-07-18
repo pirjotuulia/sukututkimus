60576accbb92243c55071088ebf6b8223eee56da
pirjotuulia <pirjoleppanen@nic.fi>
2018-07-18 21:32:26 +0300

    keskiviikon vika

--- src/main/java/gene/domain/Henkilo.java
+++ src/main/java/gene/domain/Henkilo.java
@@ -1,4 +1,51 @@
 package gene.domain;
 
+import java.time.LocalDate;
+
 public class Henkilo {
+    private String etunimi;
+    private String sukunimi;
+    private LocalDate syntymaAika;
+    private int id;
+
+    public Henkilo() {
+    }
+
+    public Henkilo(String etunimi, String sukunimi, LocalDate syntymaAika) {
+        this.etunimi = etunimi;
+        this.sukunimi = sukunimi;
+        this.syntymaAika = syntymaAika;
+    }
+
+    public String getEtunimi() {
+        return etunimi;
+    }
+
+    public void setEtunimi(String etunimi) {
+        this.etunimi = etunimi;
+    }
+
+    public String getSukunimi() {
+        return sukunimi;
+    }
+
+    public void setSukunimi(String sukunimi) {
+        this.sukunimi = sukunimi;
+    }
+
+    public LocalDate getSyntymaAika() {
+        return syntymaAika;
+    }
+
+    public void setSyntymaAika(LocalDate syntymaAika) {
+        this.syntymaAika = syntymaAika;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/gene/genealogy/GeneController.java
+++ src/main/java/gene/genealogy/GeneController.java
@@ -1,19 +1,33 @@
 package gene.genealogy;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import gene.domain.Henkilo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GeneController {
     @Value("${welcome.message:test}")
-    private String message = "Hello World";
+    private String message;
 
     @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
         model.put("message", this.message);
         return "index";
+    }
+
+    @RequestMapping("/hakutulos")
+    @ResponseBody
+    public String haku(Map<String, Object> model) {
+        List<Henkilo> henkilot = new ArrayList<>();
+        henkilot.add(new Henkilo("Pirjo", "Leppänen", LocalDate.of(1974, 2, 10)));
+        model.put("henkilot", henkilot);
+        return "hakutulos";
     }
 }
